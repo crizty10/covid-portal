0d4b36380ea2c3c8ae5ab84617a4720db3d5903d
crizty10 <83753509+crizty10@users.noreply.github.com>
2021-05-21 00:24:38 +0530

    Added urls.py

--- urls.py
+++ urls.py
@@ -1,23 +1,11 @@
-"""covidportal URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls import url
+from . import views
+
 urlpatterns = [
-    path(r'accounts/', include('django.contrib.auth.urls')),
-    path('beds/', include('beds.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('patient_reg/', views.patient_reg, name='patient_reg'),
+     path('bedallocate/', views.bedallocate, name='bedallocate'),
+    path('discharge/<int:id>', views.discharge, name='discharge'),
 ]
